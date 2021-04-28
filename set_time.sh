54d792024331eec7ceb660499e5bbe88add6a2ef
なかとも <20672947+naka-tomo@users.noreply.github.com>
2021-04-28 10:54:30 +0900

    Update set_time.sh

--- set_time.sh
+++ set_time.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo date -s "$( wget -q http://worldtimeapi.org/api/timezone/Asia/Tokyo -O - | grep -oE \"datetime\":\"[^\"]* | sed s/\"datetime\":\"// )"
+date -s "$( wget -q http://worldtimeapi.org/api/timezone/Asia/Tokyo -O - | grep -oE \"datetime\":\"[^\"]* | sed s/\"datetime\":\"// )"
